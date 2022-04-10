cdbe5b15a55d8c4a9bc652a588ccb45e3b849eeb
naga <toritonsuibu0527@gmail.com>
2022-04-11 02:18:21 +0900

    struct commit

--- ps5_connection.ino
+++ ps5_connection.ino
@@ -20,24 +20,25 @@ enum transmit_def{
   l2_VAL,
 };
 
-struct PS5_val{
-  bool circle_button = false,
-       r1_shot_spd   = false,
-       l1_shot_spd   = false;
-
-  uint8_t r_stick_x = 0,
-          r_stick_y = 0,
-          l_stick_x = 0,
-          l_stick_y = 0,
-          r2_val    = 0,
-          l2_val    = 0;
-          
-  uint8_t tx_buff[8] = {0};
-
-  void to_buff();
+enum button_def{
+  CIRCLE_BUTTON = 0x1,
+  R1_BUTTON     = 0x2,
+  L1_BUTTON     = 0X4
 };
 
-PS5_val ps;
+struct PS5_input{
+  uint8_t button = 0;
+
+  struct axis{
+    uint8_t x = 0, 
+            y = 0;
+  }r, l;
+  
+  uint8_t r2_val    = 0,
+          l2_val    = 0;
+}ps;
+
+uint8_t tx_buff[8] = {0};
 
 void setup() {
   // put your setup code here, to run once:
@@ -55,41 +56,29 @@ void loop() {
     Serial.write(ERROR_TX);
   }
 
-  if(ps.circle_button ^ PS5.getButtonPress(CIRCLE)){
-    ps.circle_button^= true;
+  if((ps.button&CIRCLE_BUTTON) ^ PS5.getButtonPress(CIRCLE)){
+    ps.button^= CIRCLE_BUTTON;
   }
-  if(ps.r1_shot_spd ^ PS5.getButtonPress(R1)){
-    ps.r1_shot_spd ^= true;
+  if((ps.button&R1_BUTTON) ^ PS5.getButtonPress(R1)<<1){
+    ps.button ^= R1_BUTTON;
   }
-  if(ps.l1_shot_spd ^ PS5.getButtonPress(L1)){
-    ps.l1_shot_spd ^= true; 
+  if((ps.button&L1_BUTTON) ^ PS5.getButtonPress(L1)<<2){
+    ps.button ^= L1_BUTTON; 
   }
 
-  ps.r_stick_x = PS5.getAnalogHat(RightHatX);
-  ps.r_stick_y = PS5.getAnalogHat(RightHatY);
-  ps.l_stick_x = PS5.getAnalogHat(LeftHatX);
-  ps.l_stick_y = PS5.getAnalogHat(LeftHatY);
+  ps.r.x = PS5.getAnalogHat(RightHatX);
+  ps.r.y = PS5.getAnalogHat(RightHatY);
+  ps.l.x = PS5.getAnalogHat(LeftHatX);
+  ps.l.y = PS5.getAnalogHat(LeftHatY);
 
   ps.r2_val = PS5.getAnalogButton(R2);
   ps.l2_val = PS5.getAnalogButton(L2);
   
 
-  if(ps.circle_button){
+  if(ps.button&CIRCLE_BUTTON){
     PS5.setRumbleOn(RumbleHigh);
     //ただの意地悪
   }
 
-  ps.to_buff();
-
-  Serial.write(ps.tx_buff, sizeof(ps.tx_buff));
-}
-
-void PS5_val::to_buff(){
-  this->tx_buff[BUTTON] = this->circle_button + (this->r1_shot_spd << 1) + (this->l1_shot_spd << 2);
-  this->tx_buff[R_STICK_X] = this->r_stick_x;
-  this->tx_buff[R_STICK_Y] = this->r_stick_y;
-  this->tx_buff[L_STICK_X] = this->l_stick_x;
-  this->tx_buff[L_STICK_Y] = this->l_stick_y;
-  this->tx_buff[R2_VAL]    = this->r2_val;
-  this->tx_buff[l2_VAL]    = this->l2_val;
+  Serial.write(tx_buff, sizeof(tx_buff));
 }
